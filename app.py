f183e244f11e927fb91b622958c61379a210cb39
Pranathi71 <146981025+Pranathi71@users.noreply.github.com>
2025-02-16 17:17:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import os
+# app.py
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Get PORT from Railway
-    app.run(host="0.0.0.0", port=port, debug=False)  # Bind to correct port
+if _name_ == "_main_":
+    app.run(debug=False)
